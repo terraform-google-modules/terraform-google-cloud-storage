f4fbf9f7c4d7e45c4d38dd767097fc27fd6db492
Luis Pena <lpena@recurly.com>
2020-03-03 10:24:47 -0800

    Added type constraints to variables

--- variables.tf
+++ variables.tf
@@ -16,10 +16,12 @@
 
 variable "project_id" {
   description = "Bucket project id."
+  type        = string
 }
 
 variable "prefix" {
   description = "Prefix used to generate the bucket name."
+  type        = string
 }
 
 variable "names" {
@@ -30,66 +32,79 @@ variable "names" {
 variable "location" {
   description = "Bucket location."
   default     = "EU"
+  type        = string
 }
 
 variable "storage_class" {
   description = "Bucket storage class."
   default     = "MULTI_REGIONAL"
+  type        = string
 }
 
 variable "force_destroy" {
   description = "Optional map of lowercase unprefixed name => boolean, defaults to false."
   default     = {}
+  type        = map
 }
 
 variable "versioning" {
   description = "Optional map of lowercase unprefixed name => boolean, defaults to false."
   default     = {}
+  type        = map
 }
 
 variable "encryption_key_names" {
   description = "Optional map of lowercase unprefixed name => string, empty strings are ignored."
   default     = {}
+  type        = map
 }
 
 variable "bucket_policy_only" {
   description = "Disable ad-hoc ACLs on specified buckets. Defaults to true. Map of lowercase unprefixed name => boolean"
   default     = {}
+  type        = map
 }
 
 variable "admins" {
   description = "IAM-style members who will be granted roles/storage.objectAdmin on all buckets."
   default     = []
+  type        = list(string)
 }
 
 variable "creators" {
   description = "IAM-style members who will be granted roles/storage.objectCreators on all buckets."
   default     = []
+  type        = list(string)
 }
 
 variable "viewers" {
   description = "IAM-style members who will be granted roles/storage.objectViewer on all buckets."
   default     = []
+  type        = list(string)
 }
 
 variable "bucket_admins" {
   description = "Map of lowercase unprefixed name => comma-delimited IAM-style bucket admins."
   default     = {}
+  type        = map
 }
 
 variable "bucket_creators" {
   description = "Map of lowercase unprefixed name => comma-delimited IAM-style bucket creators."
   default     = {}
+  type        = map
 }
 
 variable "bucket_viewers" {
   description = "Map of lowercase unprefixed name => comma-delimited IAM-style bucket viewers."
   default     = {}
+  type        = map
 }
 
 variable "labels" {
   description = "Labels to be attached to the buckets"
   default     = {}
+  type        = map
 }
 
 # we need flags to allow member lists to contain dynamic elements
@@ -97,16 +112,19 @@ variable "labels" {
 variable "set_admin_roles" {
   description = "Grant roles/storage.objectAdmin role to admins and bucket_admins."
   default     = false
+  type        = bool
 }
 
 variable "set_creator_roles" {
   description = "Grant roles/storage.objectCreator role to creators and bucket_creators."
   default     = false
+  type        = bool
 }
 
 variable "set_viewer_roles" {
   description = "Grant roles/storage.objectViewer role to viewers and bucket_viewers."
   default     = false
+  type        = bool
 }
 
 variable "lifecycle_rules" {
@@ -117,6 +135,7 @@ variable "lifecycle_rules" {
     })
     condition = map(string)
   }))
-  default     = []
   description = "List of lifecycle rules to configure. Format is the same as described in provider documentation https://www.terraform.io/docs/providers/google/r/storage_bucket.html#lifecycle_rule except condition.matches_storage_class should be a comma delimited string."
+  default     = []
+  type        = list(string)
 }
