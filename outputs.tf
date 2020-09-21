af735332c091c6fe5de082562c8d59f979421611
Adrian <agierakowski@gmail.com>
2020-09-21 21:00:37 +0100

    feat: Use for_each instead of count to create resource (#86)
    BREAKING CHANGE: The resource state locations have changed to use for_each, please see the upgrade guide for details.

--- outputs.tf
+++ outputs.tf
@@ -16,40 +16,49 @@
 
 output "bucket" {
   description = "Bucket resource (for single use)."
-  value       = google_storage_bucket.buckets[0]
+  value       = local.first_bucket
 }
 
 output "name" {
   description = "Bucket name (for single use)."
-  value       = google_storage_bucket.buckets[0].name
+  value       = local.first_bucket.name
 }
 
 output "url" {
   description = "Bucket URL (for single use)."
-  value       = google_storage_bucket.buckets[0].url
+  value       = local.first_bucket.url
 }
 
 output "buckets" {
-  description = "Bucket resources."
+  description = "Bucket resources as list."
+  value       = local.buckets_list
+}
+
+output "buckets_map" {
+  description = "Bucket resources by name."
   value       = google_storage_bucket.buckets
 }
 
 output "names" {
   description = "Bucket names."
-  value       = zipmap(var.names, slice(google_storage_bucket.buckets[*].name, 0, length(var.names)))
+  value = { for name, bucket in google_storage_bucket.buckets :
+    name => bucket.name
+  }
 }
 
 output "urls" {
   description = "Bucket URLs."
-  value       = zipmap(var.names, slice(google_storage_bucket.buckets[*].url, 0, length(var.names)))
+  value = { for name, bucket in google_storage_bucket.buckets :
+    name => bucket.url
+  }
 }
 
 output "names_list" {
   description = "List of bucket names."
-  value       = google_storage_bucket.buckets[*].name
+  value       = local.buckets_list[*].name
 }
 
 output "urls_list" {
   description = "List of bucket URLs."
-  value       = google_storage_bucket.buckets[*].url
+  value       = local.buckets_list[*].url
 }
