845a41fce19ce2d8e8c62f34b0270a5bdd03b485
Adrian Gierakowski <agierakowski@gmail.com>
2020-10-02 15:45:43 +0100

    test/setup/main.tf: add skip_gcloud_download = true

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -24,6 +24,8 @@ module "project" {
   folder_id         = var.folder_id
   billing_account   = var.billing_account
 
+  skip_gcloud_download = true
+
   activate_apis = [
     "storage-api.googleapis.com",
     "cloudresourcemanager.googleapis.com",
