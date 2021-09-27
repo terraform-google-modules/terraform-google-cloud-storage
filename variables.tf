bd1d1e7bae83e9cc1d64019f06d2ca1b9d50a67f
Philip Harvey <32467456+pharveybattelle@users.noreply.github.com>
2021-09-28 11:28:51 +1300

    feat: add additional lifecycle conditions: custom_time_before, days_since_noncurrent_time, noncurrent_time_before (#135)
    * fix missing lifecycle conditions
    
    * update documentation for missing lifecycle conditions fix
    
    * Update variables.tf
    
    * make generate_docs
    
    * make generate_docs

--- variables.tf
+++ variables.tf
@@ -188,7 +188,10 @@ variable "lifecycle_rules" {
     # - with_state - (Optional) Match to live and/or archived objects. Supported values include: "LIVE", "ARCHIVED", "ANY".
     # - matches_storage_class - (Optional) Comma delimited string for storage class of objects to satisfy this condition. Supported values include: MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, STANDARD, DURABLE_REDUCED_AVAILABILITY.
     # - num_newer_versions - (Optional) Relevant only for versioned objects. The number of newer versions of an object to satisfy this condition.
+    # - custom_time_before - (Optional) A date in the RFC 3339 format YYYY-MM-DD. This condition is satisfied when the customTime metadata for the object is set to an earlier date than the date used in this lifecycle condition.
     # - days_since_custom_time - (Optional) The number of days from the Custom-Time metadata attribute after which this condition becomes true.
+    # - days_since_noncurrent_time - (Optional) Relevant only for versioned objects. Number of days elapsed since the noncurrent timestamp of an object.
+    # - noncurrent_time_before - (Optional) Relevant only for versioned objects. The date in RFC 3339 (e.g. 2017-06-13) when the object became nonconcurrent.
     condition = map(string)
   }))
   description = "List of lifecycle rules to configure. Format is the same as described in provider documentation https://www.terraform.io/docs/providers/google/r/storage_bucket.html#lifecycle_rule except condition.matches_storage_class should be a comma delimited string."
