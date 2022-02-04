e4cee2427a4624e5d4d15f13e766a41acd9e67d0
bharathkkb <bharathkrishnakb@gmail.com>
2022-02-04 07:54:20 +0000

    prep for autodiscover example

--- examples/multiple_buckets/variables.tf
+++ examples/multiple_buckets/variables.tf
@@ -18,21 +18,3 @@ variable "project_id" {
   description = "The ID of the project in which to provision resources."
   type        = string
 }
-
-variable "names" {
-  description = "Names of the buckets to create."
-  type        = list(string)
-  default     = ["one", "two"]
-}
-
-variable "bucket_policy_only" {
-  description = "Disable ad-hoc ACLs on specified buckets. Defaults to true. Map of lowercase unprefixed name => boolean"
-  type        = map(string)
-  default     = {}
-}
-
-variable "folders" {
-  description = "Top level bucket folders. Map of lowercase unprefixed name => list of folders to create."
-  type        = map(any)
-  default     = {}
-}
