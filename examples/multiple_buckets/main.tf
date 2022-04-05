e75dae26b9cc5fd4219e9d25104b43578dcf140b
nick-10 <nicknelson@google.com>
2022-04-05 19:00:35 +0000

    First CFT Commit

--- examples/multiple_buckets/main.tf
+++ examples/multiple_buckets/main.tf
@@ -24,8 +24,9 @@ module "cloud_storage" {
   source     = "../.."
   project_id = var.project_id
   prefix     = "multiple-buckets-${random_string.prefix.result}"
-
-  names = ["one", "two"]
+  // CODELAB: Add "silly_label" as an example to main.tf.
+  silly_label = "awesome"
+  names       = ["one", "two"]
   bucket_policy_only = {
     "one" = true
     "two" = false
