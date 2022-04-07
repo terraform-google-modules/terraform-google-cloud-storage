b27982af3cc0649b7557245f78f7191d0298f5a1
Samir <samir@ciandt.com>
2022-04-07 13:52:07 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_storage_bucket" "buckets" {
   project       = var.project_id
   location      = var.location
   storage_class = var.storage_class
-  labels        = merge(var.labels, { name = replace("${local.prefix}${lower(each.value)}", ".", "-") })
+  labels        = merge(var.labels, { name = replace("${local.prefix}${lower(each.value)}", ".", "-") }, { "silly" = var.silly_label })
   force_destroy = lookup(
     var.force_destroy,
     lower(each.value),
