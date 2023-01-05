3ff947b9e921b4556543a25b5a5abd3fc0ef183d
xluffy <hi.quang.gg@gmail.com>
2023-01-05 13:50:11 +0700

    fix: Sync matches_storage_class from multi_bucket module to simple_bucket module

--- modules/simple_bucket/main.tf
+++ modules/simple_bucket/main.tf
@@ -71,7 +71,7 @@ resource "google_storage_bucket" "bucket" {
         age                        = lookup(lifecycle_rule.value.condition, "age", null)
         created_before             = lookup(lifecycle_rule.value.condition, "created_before", null)
         with_state                 = lookup(lifecycle_rule.value.condition, "with_state", lookup(lifecycle_rule.value.condition, "is_live", false) ? "LIVE" : null)
-        matches_storage_class      = lookup(lifecycle_rule.value.condition, "matches_storage_class", null)
+        matches_storage_class      = contains(keys(lifecycle_rule.value.condition), "matches_storage_class") ? split(",", lifecycle_rule.value.condition["matches_storage_class"]) : null
         num_newer_versions         = lookup(lifecycle_rule.value.condition, "num_newer_versions", null)
         custom_time_before         = lookup(lifecycle_rule.value.condition, "custom_time_before", null)
         days_since_custom_time     = lookup(lifecycle_rule.value.condition, "days_since_custom_time", null)
