534a547ead0308d39c12d023f19dc541ae1470c7
Pawan Saggu <pawansaggu007@gmail.com>
2025-03-12 09:47:34 +0000

    fix!: added concrete types for simple sa variables (#386)

--- examples/simple_bucket/main.tf
+++ examples/simple_bucket/main.tf
@@ -22,6 +22,18 @@ module "bucket" {
   project_id = var.project_id
   location   = "us"
 
+  website = {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+
+  cors = [{
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }]
+
   lifecycle_rules = [{
     action = {
       type = "Delete"
