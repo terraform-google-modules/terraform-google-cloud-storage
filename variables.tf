85bf64c8dc5d14085148cf13eb0892b4ad9286ca
pawan1210 <pawansaggu007@gmail.com>
2025-03-12 05:38:29 +0000

    fix: added concrete variable types

--- variables.tf
+++ variables.tf
@@ -193,10 +193,14 @@ variable "lifecycle_rules" {
     # Object with keys:
     # - type - The type of the action of this Lifecycle Rule. Supported values: Delete and SetStorageClass.
     # - storage_class - (Required if action type is SetStorageClass) The target Storage Class of objects affected by this Lifecycle Rule.
-    action = map(string)
+    action = object({
+      type          = string
+      storage_class = optional(string)
+    })
 
     # Object with keys:
     # - age - (Optional) Minimum age of an object in days to satisfy this condition.
+    # - send_age_if_zero - (Optional) While set true, num_newer_versions value will be sent in the request even for zero value of the field.
     # - created_before - (Optional) Creation date of an object in RFC 3339 (e.g. 2017-06-13) to satisfy this condition.
     # - with_state - (Optional) Match to live and/or archived objects. Supported values include: "LIVE", "ARCHIVED", "ANY".
     # - matches_storage_class - (Optional) Comma delimited string for storage class of objects to satisfy this condition. Supported values include: MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, STANDARD, DURABLE_REDUCED_AVAILABILITY.
@@ -207,7 +211,20 @@ variable "lifecycle_rules" {
     # - days_since_custom_time - (Optional) The number of days from the Custom-Time metadata attribute after which this condition becomes true.
     # - days_since_noncurrent_time - (Optional) Relevant only for versioned objects. Number of days elapsed since the noncurrent timestamp of an object.
     # - noncurrent_time_before - (Optional) Relevant only for versioned objects. The date in RFC 3339 (e.g. 2017-06-13) when the object became nonconcurrent.
-    condition = map(string)
+    condition = object({
+      age                        = optional(number)
+      send_age_if_zero           = optional(bool)
+      created_before             = optional(string)
+      with_state                 = optional(string)
+      matches_storage_class      = optional(string)
+      matches_prefix             = optional(string)
+      matches_suffix             = optional(string)
+      num_newer_versions         = optional(number)
+      custom_time_before         = optional(string)
+      days_since_custom_time     = optional(number)
+      days_since_noncurrent_time = optional(number)
+      noncurrent_time_before     = optional(string)
+    })
   }))
   description = "List of lifecycle rules to configure. Format is the same as described in provider documentation https://www.terraform.io/docs/providers/google/r/storage_bucket.html#lifecycle_rule except condition.matches_storage_class should be a comma delimited string."
   default     = []
@@ -218,19 +235,38 @@ variable "bucket_lifecycle_rules" {
     # Object with keys:
     # - type - The type of the action of this Lifecycle Rule. Supported values: Delete and SetStorageClass.
     # - storage_class - (Required if action type is SetStorageClass) The target Storage Class of objects affected by this Lifecycle Rule.
-    action = map(string)
+    action = object({
+      type          = string
+      storage_class = optional(string)
+    })
 
     # Object with keys:
     # - age - (Optional) Minimum age of an object in days to satisfy this condition.
+    # - send_age_if_zero - (Optional) While set true, num_newer_versions value will be sent in the request even for zero value of the field.
     # - created_before - (Optional) Creation date of an object in RFC 3339 (e.g. 2017-06-13) to satisfy this condition.
     # - with_state - (Optional) Match to live and/or archived objects. Supported values include: "LIVE", "ARCHIVED", "ANY".
-    # - matches_storage_class - (Optional) Comma delimited string for storage class of objects to satisfy this condition. Supported values include: MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, STANDARD, DURABLE_REDUCED_AVAILABILITY.
+    # - matches_storage_class - (Optional) Storage Class of objects to satisfy this condition. Supported values include: MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, STANDARD, DURABLE_REDUCED_AVAILABILITY.
+    # - matches_prefix - (Optional) One or more matching name prefixes to satisfy this condition.
+    # - matches_suffix - (Optional) One or more matching name suffixes to satisfy this condition
     # - num_newer_versions - (Optional) Relevant only for versioned objects. The number of newer versions of an object to satisfy this condition.
     # - custom_time_before - (Optional) A date in the RFC 3339 format YYYY-MM-DD. This condition is satisfied when the customTime metadata for the object is set to an earlier date than the date used in this lifecycle condition.
-    # - days_since_custom_time - (Optional) The number of days from the Custom-Time metadata attribute after which this condition becomes true.
+    # - days_since_custom_time - (Optional) Days since the date set in the customTime metadata for the object.
     # - days_since_noncurrent_time - (Optional) Relevant only for versioned objects. Number of days elapsed since the noncurrent timestamp of an object.
     # - noncurrent_time_before - (Optional) Relevant only for versioned objects. The date in RFC 3339 (e.g. 2017-06-13) when the object became nonconcurrent.
-    condition = map(string)
+    condition = object({
+      age                        = optional(number)
+      send_age_if_zero           = optional(bool)
+      created_before             = optional(string)
+      with_state                 = optional(string)
+      matches_storage_class      = optional(string)
+      matches_prefix             = optional(string)
+      matches_suffix             = optional(string)
+      num_newer_versions         = optional(number)
+      custom_time_before         = optional(string)
+      days_since_custom_time     = optional(number)
+      days_since_noncurrent_time = optional(number)
+      noncurrent_time_before     = optional(string)
+    })
   })))
   description = "Additional lifecycle_rules for specific buckets. Map of lowercase unprefixed name => list of lifecycle rules to configure."
   default     = {}
@@ -238,12 +274,20 @@ variable "bucket_lifecycle_rules" {
 
 variable "cors" {
   description = "Set of maps of mixed type attributes for CORS values. See appropriate attribute types here: https://www.terraform.io/docs/providers/google/r/storage_bucket.html#cors"
-  type        = set(any)
+  type = list(object({
+    origin          = optional(list(string))
+    method          = optional(list(string))
+    response_header = optional(list(string))
+    max_age_seconds = optional(number)
+  }))
   default     = []
 }
 
 variable "website" {
-  type        = map(any)
+  type = object({
+    main_page_suffix = optional(string)
+    not_found_page   = optional(string)
+  })
   default     = {}
   description = "Map of website values. Supported attributes: main_page_suffix, not_found_page"
 }
